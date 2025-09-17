50eb4bd9c982050efa0301da7d3b1b50ef1937aa
hailey <me@haileyok.com>
2025-09-17 14:29:05 -0700

    fix: make kafka client ids unique by hostname (#6)

--- osprey_worker/src/osprey/worker/sinks/__init__.py
+++ osprey_worker/src/osprey/worker/sinks/__init__.py
@@ -8,6 +8,8 @@ Therefore we patch at the top of this file -- it's generally fine to double patc
 but not ideal as clients should be responsible for patching
 """
 
+import platform
+
 from osprey.worker.lib.patcher import patch_all
 
 patch_all()
@@ -142,10 +144,14 @@ def get_rules_sink_input_stream(
         return StaticInputStream(random_actions)
     elif input_stream_source == InputStreamSource.KAFKA:
         config = CONFIG.instance()
-        client_id = config.get_str('OSPREY_KAFKA_INPUT_STREAM_CLIENT_ID', 'localhost')
+        client_id = config.get_str('OSPREY_KAFKA_INPUT_STREAM_CLIENT_ID', platform.node())
+        client_id_suffix = config.get_optional_str('OSPREY_KAFKA_INPUT_STREAM_CLIENT_ID_SUFFIX')
         input_topic: str = config.get_str('OSPREY_KAFKA_INPUT_STREAM_TOPIC', 'osprey.actions_input')
         input_bootstrap_servers: list[str] = config.get_str_list('OSPREY_KAFKA_BOOTSTRAP_SERVERS', ['localhost'])
         group_id = config.get_optional_str('OSPREY_KAFKA_GROUP_ID')
+
+        if client_id_suffix:
+            client_id = f'{client_id}-{client_id_suffix}'
 
         consumer: PatchedKafkaConsumer = PatchedKafkaConsumer(
             input_topic,
