ef42df97471b2b84049dddf3475d3a686daa66ba
Hailey <me@haileyok.com>
2025-09-18 17:58:52 -0700

    dont treat stream like an array

--- osprey_worker/src/osprey/worker/adaptor/plugin_manager.py
+++ osprey_worker/src/osprey/worker/adaptor/plugin_manager.py
@@ -85,9 +85,9 @@ def bootstrap_action_proto_deserializer() -> ActionProtoDeserializer | None:
 def bootstrap_input_stream() -> BaseInputStream[BaseAckingContext[Action]] | None:
     load_all_osprey_plugins()
 
-    streams = plugin_manager.hook.register_input_stream()
-    if streams:
-        # spec has firstresult=True set, so at most it will be one.
-        return streams[0]
+    stream = plugin_manager.hook.register_input_stream()
+    if stream:
+        # spec has firstresult=True set, so it will return the first registered stream
+        return stream
     else:
         return None
