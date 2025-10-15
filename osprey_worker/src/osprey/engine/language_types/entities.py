1505cd660ac1260d218f2ed8cffdc9f7e6a926c6
ayu <ayu@ayu.dev>
2025-10-15 23:34:32 +0000

    make entityt pydantically valitabable

--- osprey_worker/src/osprey/engine/language_types/entities.py
+++ osprey_worker/src/osprey/engine/language_types/entities.py
@@ -42,6 +42,22 @@ class EntityT(OspreyInvariantGeneric[_T], PostExecutionConvertible[_T]):
         # the real type. Assumes that this has no subclasses.
         return typing_inspect.get_args(cls)[0]
 
+    @classmethod
+    def __get_validators__(cls):
+        """Pydantic v1 validator"""
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        """Validate and convert to EntityT"""
+        if isinstance(v, cls):
+            return v
+        if isinstance(v, dict):
+            if 'type' in v and 'id' in v:
+                return cls(type=v['type'], id=v['id'])
+            raise TypeError(f'EntityT expects dict with "type" and "id" keys, got {v}')
+        raise TypeError(f'EntityT expected EntityT or dict; got {type(v)}')
+
 
 # Make sure this looks as it's used in the Osprey language when used in error messages.
 EntityT.__name__ = 'Entity'
