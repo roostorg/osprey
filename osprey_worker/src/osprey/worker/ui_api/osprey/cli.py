cd650af96a0f4e378972f93f44e6bfff6793ff7e
Hailey <me@haileyok.com>
2025-10-16 12:35:10 -0700

    tidy

--- osprey_worker/src/osprey/worker/ui_api/osprey/cli.py
+++ osprey_worker/src/osprey/worker/ui_api/osprey/cli.py
@@ -1,5 +1,4 @@
 # ruff: noqa: E402
-from google.cloud import storage
 from osprey.worker.lib.patcher import patch_all  # isort: skip
 
 patch_all()  # please ensure this occurs before *any* other imports !
@@ -84,8 +83,6 @@ def export_all_actions(
         return {'id': d['id'], 'timestamp': d['timestamp'], 'action_data': d['action_data']}
 
     storage_service = bootstrap_execution_result_storage_service()
-    if not storage_service:
-        raise
     druid_result = query_druid()
     while druid_result.action_ids:
         events = storage_service.get_many(action_ids=druid_result.action_ids)
