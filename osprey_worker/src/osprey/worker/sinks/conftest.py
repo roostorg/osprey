5e806d6ceaa0e2adc6f3214db8d996d21f14f0e1
Caidan Williams <caidan@internet.dev>
2025-10-17 18:17:02 -0700

    test(config): move config_setup fixture to worker/conftest.py for better test isolation
    Relocate the config_setup pytest fixture from lib/conftest.py and sinks/conftest.py into worker/conftest.py to centralize configuration setup and teardown for tests. This improves test isolation and reduces duplication.

--- osprey_worker/src/osprey/worker/sinks/conftest.py
+++ osprey_worker/src/osprey/worker/sinks/conftest.py
@@ -3,21 +3,7 @@ from osprey.worker.lib.patcher import patch_all
 
 patch_all(patch_gevent=False, patch_ddtrace=False)  # please ensure this occurs before *any* other imports !
 
-from typing import Any, Generator
 
-import pytest
-from osprey.worker.lib.singletons import CONFIG
 from osprey.worker.lib.tests import test_utils
 
 postgres_database_config = test_utils.make_postgres_database_config_fixture()
-
-
-@pytest.fixture(autouse=True)  # autouse = True means automatically use for each test
-def config_setup() -> Generator[Any, None, None]:
-    CONFIG.instance().configure_from_env()
-    # yield is used here to basically split this function into two parts:
-    # all code before `yield` is the setup code (run before each test), and
-    # all code after `yield` is the teardown code (run after each test)
-    yield  # this line is where the testing happens
-    # teardown code
-    CONFIG.instance().unconfigure_for_tests()
