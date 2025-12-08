2ea38adc41c6eead3f7ab4d58f0d3654dab2dd98
Leon Shi <101139283+cmttt@users.noreply.github.com>
2025-12-08 10:14:48 -0800

    Try last for hookimpl_osprey (#84)

--- osprey_worker/src/osprey/worker/_stdlibplugin/storage_register.py
+++ osprey_worker/src/osprey/worker/_stdlibplugin/storage_register.py
@@ -1,10 +1,12 @@
+from typing import Optional
+
 from osprey.worker.adaptor.plugin_manager import hookimpl_osprey
 from osprey.worker.lib.config import Config
 from osprey.worker.lib.storage.stored_execution_result import ExecutionResultStore, StoredExecutionResultMinIO
 
 
-@hookimpl_osprey
-def register_execution_result_store(config: Config) -> ExecutionResultStore:
+@hookimpl_osprey(trylast=True)
+def register_execution_result_store(config: Config) -> Optional[ExecutionResultStore]:
     endpoint = config.get_str('OSPREY_MINIO_ENDPOINT', 'minio:9000')
     access_key = config.get_str('OSPREY_MINIO_ACCESS_KEY', 'minioadmin')
     secret_key = config.get_str('OSPREY_MINIO_SECRET_KEY', 'minioadmin123')
