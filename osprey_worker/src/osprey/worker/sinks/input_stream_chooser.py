434eb4bf89c395706319ebcc6e4348ab0984714c
hailey <hailey.elizabeth@discordapp.com>
2026-04-12 22:50:12 +0000

    replace consumer

--- osprey_worker/src/osprey/worker/sinks/input_stream_chooser.py
+++ osprey_worker/src/osprey/worker/sinks/input_stream_chooser.py
@@ -3,7 +3,7 @@ import random
 from datetime import datetime, timedelta
 
 from google.cloud import pubsub_v1
-from kafka.coordinator.assignors.roundrobin import RoundRobinPartitionAssignor
+from confluent_kafka import Consumer
 from osprey.engine.executor.execution_context import Action
 from osprey.worker.adaptor.plugin_manager import bootstrap_input_stream
 from osprey.worker.lib.singletons import CONFIG
@@ -15,7 +15,6 @@ from osprey.worker.sinks.sink.input_stream import (
 )
 from osprey.worker.sinks.sink.osprey_coordinator_input_stream import OspreyCoordinatorInputStream
 from osprey.worker.sinks.utils.acking_contexts import BaseAckingContext, NoopAckingContext
-from osprey.worker.sinks.utils.kafka import PatchedKafkaConsumer
 
 
 def get_rules_sink_input_stream(
@@ -113,13 +112,17 @@ def get_rules_sink_input_stream(
         if client_id_suffix:
             client_id = f'{client_id}-{client_id_suffix}'
 
-        consumer: PatchedKafkaConsumer = PatchedKafkaConsumer(
-            input_topic,
-            bootstrap_servers=input_bootstrap_servers,
-            client_id=client_id,
-            group_id=group_id,
-            partition_assignment_strategy=(RoundRobinPartitionAssignor,),
-        )
+        consumer_config = {
+            'bootstrap.servers': ','.join(input_bootstrap_servers),
+            'client.id': client_id,
+            'group.id': group_id or 'osprey-consumer',
+            'partition.assignment.strategy': 'roundrobin',
+            'auto.offset.reset': 'latest',
+        }
+
+        consumer = Consumer(consumer_config)
+        consumer.subscribe([input_topic])
+
         from osprey.worker.sinks.sink.input_stream import KafkaInputStream
 
         return KafkaInputStream(
