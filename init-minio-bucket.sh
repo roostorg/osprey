7a3951dbdabd71a760964cd4680d66b5c18694eb
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2026-09-22 00:17:52 +0200

    Replace Minio `mc` with `rc` the RustFS CLI, since `mc` was yanked from the registry

--- init-minio-bucket.sh
+++ init-minio-bucket.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-mc alias set myminio http://minio:9000 minioadmin minioadmin123
-mc mb --ignore-existing myminio/execution-output
+rc alias set myminio http://minio:9000 minioadmin minioadmin123
+rc mb --ignore-existing myminio/execution-output
 echo "Bucket 'execution-output' ready"
